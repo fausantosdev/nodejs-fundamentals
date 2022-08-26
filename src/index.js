57013275ec03984b716cdf142b8a94da9eea4fbd
fausantosdev <fausantosdev@hotmail.com>
2022-08-25 21:56:42 -0300

    :construction: add user creation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    return res.json({ ok: true })
+
+const costumers = []
+
+app.post('/sign-up', (req, res) => {
+    const { name, cpf } = req.body
+
+    const cpfAlreadyExists = costumers.some((costumer) => costumer.cpf === cpf)
+
+    if(cpfAlreadyExists){
+        return res.status(400).json({ ok: false })
+    }
+
+    costumers.push({
+        id: uuidv4(),
+        cpf,
+        name,
+        statement: []
+    })
+
+    return res.status(201).json({ ok: true })
 })
 
 app.listen(3333, () => {
